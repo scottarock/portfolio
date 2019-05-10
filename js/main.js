0250cf54c9ff7ef27bcc8bf7e447c5e47e6dfcf2
Scott Rock <scottrockbiz@gmail.com>
2019-05-10 11:03:20 -0700

    more content added, sidenav added

--- js/main.js
+++ js/main.js
@@ -1,6 +1,25 @@
 $(document).ready(function() {
-  console.log('jquery running');
-  $('.nav-wrapper ul li').on('click', function(eventObject) {
-    console.log('nav clicked', eventObject);
-  })
+
+  // initialize Materialize objects
+  $('.sidenav').sidenav();
+
+  // get instances of Materialize objects
+  var mobileNav = M.Sidenav.getInstance($('.sidenav'));
+
+  // set the active link in the navbar
+  $('.nav-wrapper ul li').on('click', function(event) {
+    $('.nav-wrapper ul li').siblings().removeClass('active');
+    event.currentTarget.classList.add('active');
+  });
+
+  // clear active links when logo logo clicked
+  $('.brand-logo').on('click', function(event) {
+    $('.nav-wrapper ul li').siblings().removeClass('active');
+  });
+
+  // hide sidenav bar when link clicked
+  $('.sidenav li').on('click', function(event) {
+    mobileNav.close();
+  });
+
 });
